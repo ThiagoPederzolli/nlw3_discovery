ab8aeb193d5955c81919234192727bc3fa2b7d47
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-15 10:41:54 -0300

    usando hbs para apresentar dados em tela

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -7,16 +7,31 @@ const icon = L.icon({
   iconSize: [58, 68],
   incnAnchor: [29, 68],
   popupAnchor: [170, 2]
+});
+
+function addMarker({id, name, lat, lng}) {
+  
+  const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+  }).setContent(`${name} <a href="/orphanage?id=${id}" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>`);
+  
+  
+  L.marker([lat,lng], { icon })
+  .addTo(map)
+  .bindPopup(popup);
+}
+
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+orphanagesSpan.forEach(span => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+  addMarker(orphanage)
 })
-
-const popup = L.popup({
-  closeButton: false,
-  className: 'map-popup',
-  minWidth: 240,
-  minHeight: 240
-}).setContent('Lar de Menores <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
-
-
-L.marker([-31.7534021,-52.3350085], { icon })
-.addTo(map)
-.bindPopup(popup);
