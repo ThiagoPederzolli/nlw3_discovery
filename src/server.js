d8fad9504ed67facbd7db4f2e3ea8ed0ab2be8b9
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-15 08:32:38 -0300

    iniciando primeira rota e corrigindo caminhos

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('Olá Mundão!')
+const express = require('express');
+const server = express();
+const path = require('path');
+
+server
+.use(express.static('public'))
+.get('/', (request, response) => {
+  return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+});
+
+server.listen(5500);
+
