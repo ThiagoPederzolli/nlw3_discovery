1579defdd5d2ea2451c067108d9255f3bb02db44
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-10-18 20:49:10 -0300

    concluido cadastro pelo front

--- src/pages.js
+++ src/pages.js
@@ -38,4 +38,36 @@ module.exports = {
   createOrphanage(req, res) {
     return res.render('create-orphanage');
   },
+
+  async saveOrphanage(req, res) {
+    const fields = req.body
+
+    if(Object.values(fields).includes('')){
+      return res.send('Todos os campos devem ser preenchidos');
+    }
+
+
+    try {
+      const db = await Database;
+      await saveOrphanage(db, {
+        lat: fields.lat,
+        lng: fields.lng,
+        name: fields.name,
+        about: fields.about,
+        whatsapp: fields.whatsapp.toString(),
+        images: fields.images,
+        instructions: fields.instructions,
+        opening_hours: fields.opening_hours,
+        open_on_weekends: fields.open_on_weekends,
+        
+      })
+
+      return res.redirect('/orphanages')
+      
+    } catch (error) {
+      console.log(error);
+      return res.send('Erro no banco de dados!')
+    }
+   
+  }
 }
